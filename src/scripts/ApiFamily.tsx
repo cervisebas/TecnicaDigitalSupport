115fcce83c5eebf8bb0de905294baafcbb3a6387
Sebastian Cerviño <cervisebas@gmail.com>
2022-08-18 15:02:19 -0300

    18/08/2022 15:02hs

--- src/scripts/ApiFamily.tsx
+++ src/scripts/ApiFamily.tsx
@@ -1,8 +1,8 @@
-import { encode } from "base-64";
 import FamilySystem from "./family";
 import PWAServise from "./PWA";
+import { keyCodeFamily } from "./SecurityKeyCode";
 
-const keyCodeFamily: string = encode('k3Ra4Q3HAL9MR7SAEPSNGY3mQNWsvWY2pLdLcu5LesH8rx6g2EFsrFAuCxsShbV7');
+//const keyCodeFamily: string = encode('k3Ra4Q3HAL9MR7SAEPSNGY3mQNWsvWY2pLdLcu5LesH8rx6g2EFsrFAuCxsShbV7');
 const urlBase: string = 'https://tecnica-digital.ga';
 
 const Family = new FamilySystem(urlBase, keyCodeFamily);
